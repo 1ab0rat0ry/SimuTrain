2eae7f9583291441f31fd67ca55f7562a7d1824e
1ab0rat0ry <192498930+1ab0rat0ry@users.noreply.github.com>
2025-09-17 18:18:05 +0200

    Vehicle, Consist: cleanup code

--- src/simulation/rollingstock/Consist.lua
+++ src/simulation/rollingstock/Consist.lua
@@ -1,44 +1,34 @@
----@type ArrayList
-local ArrayList = require "Assets/1ab0rat0ry/SimuTrain/src/utils/ArrayList.out"
-
---local MIN_TIME_STEP = 0.04
-
----@class Consist
----@field public length number
----@field public vehicleCount number
----@field public vehicles ArrayList
-local Consist = {
-    length = 0,
-    vehicleCount = 0,
-    vehicles = ArrayList
-}
+--- @class Consist
+--- @field public length number
+--- @field public vehicleCount number
+--- @field public vehicles table<number, Vehicle>
+local Consist = {}
 Consist.__index = Consist
 
----@return Consist
+--- @return Consist
 function Consist:new()
-    ---@type Consist
+    --- @type Consist
     local instance = {
         length = 0,
         vehicleCount = 0,
-        vehicles = ArrayList:new()
+        vehicles = {}
     }
-    instance = setmetatable(instance, self)
 
-    return instance
+    return setmetatable(instance, self)
 end
 
----Adds vehicle to the end.
----@param vehicle Vehicle
+--- Adds vehicle to the end.
+--- @param vehicle Vehicle
 function Consist:addVehicle(vehicle)
     if self.vehicleCount > 0 then
-        self.vehicles.elements[self.vehicleCount].brakePipe:setRear(vehicle.brakePipe)
+        self.vehicles[self.vehicleCount].brakePipe:setRear(vehicle.brakePipe)
     end
     self.vehicleCount = self.vehicleCount + 1
-    self.vehicles:add(vehicle)
+    self.vehicles[self.vehicleCount] = vehicle
 end
 
----Updates all vehicles in consist and propagates brake pipe.
----@param deltaTime number
+--- Updates all vehicles in consist and propagates brake pipe.
+--- @param deltaTime number
 function Consist:update(deltaTime)
     --- @param i number
     --- @param vehicle Vehicle
@@ -54,20 +44,23 @@ function Consist:update(deltaTime)
     end
 end
 
-        --for _, vehicle in ipairs(self.vehicles.elements) do
-        --    vehicle:update(deltaTime)
-        --end
-        --time = time + fixedDeltaTime
-    --end
+--- @return Vehicle
+function Consist:getFirstVehicle()
+    return self.vehicles[1]
 end
 
----Calculates brake force of the whole consist.
----@return number brake force as number between `0` and `1`
+--- @return Vehicle
+function Consist:getLastVehicle()
+    return self.vehicles[self.vehicleCount]
+end
+
+--- Calculates brake force of the whole consist.
+--- @return number brake force as number between `0` and `1`
 function Consist:getBrakeControl()
     local brakeControlSum = 0
 
-    for _, vehicle in ipairs(self.vehicles.elements) do
-        brakeControlSum = brakeControlSum + vehicle:getBrakeControl()
+    for i = 1, self.vehicleCount do
+        brakeControlSum = brakeControlSum + self.vehicles[i]:getBrakeControl()
     end
 
     return brakeControlSum / self.vehicleCount
--- src/simulation/rollingstock/Vehicle.lua
+++ src/simulation/rollingstock/Vehicle.lua
@@ -1,26 +1,25 @@
----@type Reservoir
+--- @type Reservoir
 local Reservoir = require "Assets/1ab0rat0ry/SimuTrain/src/simulation/brake/common/Reservoir.out"
----@type Pipe
+--- @type Pipe
 local Pipe = require "Assets/1ab0rat0ry/SimuTrain/src/simulation/brake/common/Pipe.out"
----@type MathUtil
+--- @type MathUtil
 local MathUtil = require "Assets/1ab0rat0ry/SimuTrain/src/utils/math/MathUtil.out"
 
-local PIPE_DIAMETER = 0.03175 --metres
-local PIPE_CROSS_SECTION_AREA = math.pi * PIPE_DIAMETER ^ 2 / 4 --metres squared
-local HOSE_LENGTH = 0.5 --metres
+local PIPE_DIAMETER = 0.03175 -- [m]
+local PIPE_CROSS_SECTION_AREA = math.pi * PIPE_DIAMETER ^ 2 / 4 -- [m²]
+local HOSE_LENGTH = 0.62 -- [m]
 
----@class Vehicle
----@field private length number
----@field private pipeCapcity number
----@field public brakePipe Pipe
----@field private feedPipe Reservoir
----@field private brakeValve table
----@field private distributor table
----@field private accelerator table
----@field public distributorRes Reservoir
----@field public auxiliaryRes Reservoir
----@field public controlRes Reservoir
----@field public cylinder Reservoir
+--- @class Vehicle
+--- @field private length number
+--- @field public brakePipe Pipe
+--- @field public feedPipe Reservoir
+--- @field private brakeValve table
+--- @field private distributor table
+--- @field private accelerator table
+--- @field public distributorRes Reservoir
+--- @field public auxiliaryRes Reservoir
+--- @field public controlRes Reservoir
+--- @field public cylinder Reservoir
 local Vehicle = {
     length = 0,
     --brakeBlocks = 0,
@@ -40,13 +39,13 @@ local Vehicle = {
 }
 Vehicle.__index = Vehicle
 
----@param length number
----@return Vehicle
+--- @param length number
+--- @return Vehicle
 function Vehicle:new(length)
-    ---@type Vehicle
+    --- @type Vehicle
     local instance = {
         length = length,
-        brakePipe = Pipe:new(length + 4 * HOSE_LENGTH, PIPE_DIAMETER),
+        brakePipe = Pipe:new(length + 2 * HOSE_LENGTH, PIPE_DIAMETER),
         feedPipe = nil,
         brakeValve = nil,
         distributor = nil,
@@ -60,16 +59,16 @@ function Vehicle:new(length)
     return setmetatable(instance, self)
 end
 
----Updates all systems and devices of vehicle.
----@param deltaTime number
+--- Updates all systems and devices of vehicle.
+--- @param deltaTime number
 function Vehicle:update(deltaTime)
     if self.brakeValve then self.brakeValve:update(deltaTime, self.feedPipe, self.brakePipe) end
     if self.distributor then self.distributor:update(deltaTime) end
     --if self.accelerator then self.accelerator:update(deltaTime, self.brakePipe, self.distributor) end
 end
 
----Calculates brake force.
----@return number brake force expressed as number between `0` and `1`
+--- Calculates brake force.
+--- @return number brake force expressed as number between `0` and `1`
 function Vehicle:getBrakeControl()
     return MathUtil.inverseLerp(self.cylinder:getManoPressure(), 0.3, 3.8)
 end
@@ -78,22 +77,22 @@ function Vehicle:setupBrakeSystem()
     
 end
 
----@param mainResCapacity number
+--- @param mainResCapacity number
 function Vehicle:addFeedPipe(mainResCapacity)
-    self.feedPipe = Reservoir:new((self.length + 4 * HOSE_LENGTH) * PIPE_CROSS_SECTION_AREA + (mainResCapacity or 0))
+    self.feedPipe = Reservoir:new(self.length * PIPE_CROSS_SECTION_AREA + (mainResCapacity or 0))
 end
 
----@param distributor table
+--- @param distributor table
 function Vehicle:addDistributor(distributor)
     self.distributor = distributor
 end
 
----@param brakeValve table
+--- @param brakeValve table
 function Vehicle:addBrakeValve(brakeValve)
     self.brakeValve = brakeValve
 end
 
----@param accelerator table
+--- @param accelerator table
 function Vehicle:addAccelerator(accelerator)
     self.accelerator = accelerator
 end
